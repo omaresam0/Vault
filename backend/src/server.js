e6b4926eb0bb09277a638ab49df367e198fa61d8
omaresam0 <omaresam2000@hotmail.com>
2026-01-01 20:13:56 +0200

    changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,34 @@ import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-
+import path from "path";
 import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
-// middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    // middleware
+    app.use(cors({
     origin: "http://localhost:5173"
-}));
+    })
+);
+}
+
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")) // display index.html
+})
+}
+
 
 connectDB().then(() => {
     app.listen(PORT, () =>{
